9b29e1efb621ef55b202d13a449ea0c1fdedfd7c
imanroem <97122739+imanroem@users.noreply.github.com>
2022-06-20 12:25:58 +0900

    img

---
+++ script.js
@@ -0,0 +1,11 @@
+alert('Selamat Datang....')
+var cek = true;
+
+while(cek === true) {
+    var nama = prompt('masukan nama : ');
+    alert('Hallo Ngabersss ' + nama + ' :v');
+
+    cek = confirm('Sudah benar namamu? Coba lagi?');
+}
+
+alert('Tarimakasih :)')
