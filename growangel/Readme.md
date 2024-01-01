fcb831b123a48d02b58d904b9e2357bb12d1d276
MuellerGTPS <97122739+muellergtps@users.noreply.github.com>
2024-01-01 17:06:36 +0900

    GrowAngel-PS || 2024[New Year]

---
+++ growangel/Readme.md
@@ -0,0 +1,1 @@
+jan di jual pea
---
+++ growangel/config.js
@@ -0,0 +1,12 @@
+let option = {
+    server_name: "GrowAngel-PS",
+    server_description: "deskripsi",
+    owner: [
+        "@MuellerGTPS"
+    ],
+    discord: "/growangel.txt",
+    whatsapp: "https://chat.whatsapp.com/J5mtHM9cNfg4GGREP7RvoR",
+    ip_address: "192.53.118.126",
+    host_for_android: "growangel.txt"
+}
+
