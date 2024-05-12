167babd3032fde913f58217fd6ca21f32de2bcd5
MuellerGTPS <97122739+muellergtps@users.noreply.github.com>
2024-05-13 04:08:02 +0900

    emps

---
+++ emeraldps/config.js
@@ -0,0 +1,16 @@
+let option = {
+    server_name: "Emerald-PS",
+    server_description: "deskripsi",
+    owner: [
+        "@Brandon & @Zidan"
+    ],
+    coder: [
+        "@MuellerGTPS, @Adip, @Adip, @Gans"
+    ], 
+    discord: "/emeraldps.txt",
+    whatsapp: "https://chat.whatsapp.com/HUyQgEAQLWy08KHMD6Ozsw",
+    ip_address: "134.209.156.20",
+    power_tunnel: "https://gtps.fun/EMPS.txt",
+    host_for_android: "emeraldps.txt"
+}
+
